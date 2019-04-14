33863998c1a636a9a185466ba95f0199726601eb
js-tut <johnsmilga@gmail.com>
2019-04-13 19:50:47 -0700

    SetFlex and SetBackground Helper functions

--- src/styles.js
+++ src/styles.js
@@ -10,3 +10,15 @@ export const setFont = {
   main: "font-family: 'Lato', sans-serif;",
   slanted: "font-family: 'Courgette', cursive;"
 };
+
+export const setFlex = ({ x = "center", y = "center" } = {}) => {
+  return `display:flex;align-items:${y};justify-content:${x}`;
+};
+
+export const setBackground = ({
+  img = "https://images.pexels.com/photos/1628086/pexels-photo-1628086.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+  color = "rgba(0,0,0,0)"
+} = {}) => {
+  return `background: linear-gradient(${color}, ${color}),
+    url(${img}) center/cover fixed no-repeat`;
+};
