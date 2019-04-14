80ddc773c1479342bcaa2658ce37bf3fd5774b6a
js-tut <johnsmilga@gmail.com>
2019-04-13 20:32:48 -0700

    Banner Component Basics

--- src/components/home/Header.js
+++ src/components/home/Header.js
@@ -2,11 +2,15 @@ import React from "react";
 import Hero from "../globals/Hero";
 import aboutImg from "../../images/aboutBcg.jpeg";
 import homeImg from "../../images/homeBcg.jpeg";
-
+import Banner from "../globals/Banner";
 const Header = () => {
   return (
     <Hero img={homeImg}>
-      <h1>hello world</h1>
+      <Banner
+        greeting="welcome to"
+        title="beachwalk resort"
+        text="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi provident eos sapiente deleniti in quisquam."
+      />
     </Hero>
   );
 };
