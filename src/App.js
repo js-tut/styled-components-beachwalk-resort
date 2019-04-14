1ea14203c3cd1342c6bfda7ce5f7e6f3eb164d58
js-tut <johnsmilga@gmail.com>
2019-04-13 18:23:20 -0700

    Global Styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
 import Home from "./pages/Home";
+import GlobalStyles from "./components/globals/GlobalStyles";
 class App extends Component {
   render() {
     return (
       <React.Fragment>
+        <GlobalStyles />
         <Home />
       </React.Fragment>
     );
